401e3827fc77a8fad116ffc589ee3cf3422a81de
chloet <chloe.t@krustuniverse.com>
2022-03-09 17:02:03 +0800

    succesfully started express backend

--- backend/controllers/faucetController.js
+++ backend/controllers/faucetController.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+const fs = require('fs')
+const Caver = require('caver-js')
+
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
@@ -5,7 +9,12 @@ const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 
+const caver = new Caver(process.env.BAOBAB_URL)
+// console.log('caver: ', caver)
+
 app.post('/sendKlay', (req, res) => {
   console.log('wallet address: ', req.body)
   res.send('send klay')
 })
+
+module.exports = app
