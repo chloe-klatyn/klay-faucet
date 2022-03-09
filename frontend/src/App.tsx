aa050374bbbef914a4a4a021ee08cbfbda93586e
chloet <chloe.t@krustuniverse.com>
2022-03-09 17:28:53 +0800

    added intro panel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Intro from './components/Intro'
 import AddressInput from './components/AddressInput'
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen justify-between">
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <AddressInput />
+      <div>
+        <Intro />
+        <AddressInput />
+      </div>
       <Footer />
     </div>
   )
---
+++ frontend/src/components/Intro.tsx
@@ -0,0 +1,9 @@
+const Intro = () => {
+  return (
+    <div className="flex justify-center text-2xl font-bold place-content-between p-3 items-center text-gray-900">
+      <div>Baobab Faucet</div>
+    </div>
+  )
+}
+
+export default Intro
