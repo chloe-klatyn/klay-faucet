753a78f3925db76346f7ed130b288fe31acd8de0
chloet <chloe.t@krustuniverse.com>
2022-03-09 16:54:48 +0800

    added faucet logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,12 @@
 const Header = () => {
-  return <header>xxx</header>
+  return (
+    <header>
+      <div className="flex place-content-between p-3 items-center text-gray-900 bg-gray-100">
+        <div>Logo</div>
+        <div>Wallet Address</div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
