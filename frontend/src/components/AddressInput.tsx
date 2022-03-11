6d06d4ba59bac44246797975ba3468c153756ab0
chloet <chloe.t@krustuniverse.com>
2022-03-11 15:47:49 +0800

    added toast with link to baobab klaytnscope

--- frontend/src/components/AddressInput.tsx
+++ frontend/src/components/AddressInput.tsx
@@ -1,6 +1,8 @@
 import { useForm } from 'react-hook-form'
 import { useState, useContext } from 'react'
 import providerContext from '../context/context'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 type FormData = {
   recipient: string
@@ -30,10 +32,27 @@ const Input = () => {
       )
       const res = await response.json()
       console.log('res: ', res)
+      if (res.code === 993) {
+        console.error('Error requesting funds')
+        toast.error('You have reached the request limit. Please try again later.', {
+          theme: 'colored',
+        })
+      } else if (res.code === 0) {
+        const tx = res.data.replace(/['"]+/g, '')
+        toast.success(toastMessage(tx), { theme: 'colored' })
+      }
     } catch (err: any) {
       console.error(err.message)
     }
   }
+
+  const toastMessage = (txcode: string) => (
+    <div className="underline ">
+      <a href={`https://baobab.scope.klaytn.com/tx/${txcode}`} target="_blank">
+        Success! View your transaction here
+      </a>
+    </div>
+  )
 
   const validateAddress = (input: any) => {
     const prefix = input.slice(0, 2)
@@ -45,6 +64,17 @@ const Input = () => {
 
   return (
     <div className="flex flex-col items-center w-full">
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div className="place-content-center font-body mb-6 tracking-widest w-1/3">
         <label className="mb-4 text-grey-600">Enter KLAY address</label>
         <div className="flex space-x-2">
